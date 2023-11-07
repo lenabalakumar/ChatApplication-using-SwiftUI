cee586a6d00ac293a093c61acaec642465c171de
Lenabalakumar Subbarayan <lenabalakumar@gmail.com>
2023-11-07 15:11:39 +0000

    Initial Commit

---
+++ ChatApplication/ChatApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApplicationApp.swift
+//  ChatApplication
+//
+//  Created by Lenabalakumar Subbarayan on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
